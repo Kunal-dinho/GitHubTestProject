a69a8f761dda4bb4ddb8d51b671d505723502443
Kunal <kunalashirodkar@gmail.com>
2021-04-23 11:29:17 +0530

    First

---
+++ GitHubProject/src/com/github/GitHub.java
@@ -0,0 +1,11 @@
+package com.github;
+
+public class GitHub {
+	
+	public static void main(String args[]) {
+		System.out.println("........GIT........HUB........TEST........");
+		int l=5,b=7;
+		System.out.println("Area "+ (l*b));
+		
+	}
+}
